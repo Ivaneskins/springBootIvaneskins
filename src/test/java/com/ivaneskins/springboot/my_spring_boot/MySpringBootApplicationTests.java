3858ff4a3d8aa4e1c49656bceddd31c6d0888eb0
Ivaneskins <weina2004@mail.ru>
2023-08-18 08:31:50 +0500

    Done

---
+++ src/test/java/com/ivaneskins/springboot/my_spring_boot/MySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivaneskins.springboot.my_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
